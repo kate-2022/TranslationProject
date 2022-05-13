6a986808a65f98aa639d2b3bc5fc2cb5314aaad9
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-05-13 13:18:11 +0200

    Update Methionin.java

--- Methionin.java
+++ Methionin.java
@@ -1,6 +1,4 @@
-import java.awt.event.ActionEvent;
 import java.util.ArrayList;
-// import java.util.LinkedList;
 
 public class Methionin extends AminoAcids implements Translatable {
 	
@@ -26,13 +24,13 @@ public class Methionin extends AminoAcids implements Translatable {
 	}
 	                                                                                      // toString()-Methode anpassen!! s.u.
 	
-	public String compareSub(ArrayList<String>codons, String peptide) {                   // throws StackOverflowError{ 
+	public String compareSub(ArrayList<String>codons, String peptide) {                 
 	  pull.getCodons();
 	  
-	  if (i == codons.size()-1) {    // für alle AS anpassen!!!
+	  if (i == codons.size()-1) {                    
 		    System.out.print(peptide.toString());   
 	  }
-	  while( i < codons.size()-1) {  // size-1 für alle AS anpassen!!!
+	  while( i < codons.size()-1) {                  
 		  if (codons.get(i).equals("AUG")) {                                
 			     singleLetterCode = "M";                       
 			     proteinSequence(peptide, singleLetterCode);
@@ -51,7 +49,7 @@ public class Methionin extends AminoAcids implements Translatable {
 	
 
 	@Override
-    public String toString() {                        // das hier noch überprüfen und bei allen AS Klassen anpassen!!
+    public String toString() {                        // das hier noch Ã¼berprÃ¼fen und bei allen AS Klassen anpassen!!
 		peptide = peptide + singleLetterCode;
 		return peptide;
     }
