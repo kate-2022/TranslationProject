62a51eb3fae7c16f3bfb26a7bf258bc41ef65b17
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-05-16 21:27:24 +0200

    Update Cystein.java

--- Cystein.java
+++ Cystein.java
@@ -22,11 +22,11 @@ public class Cystein extends AminoAcids implements Translatable {
 	
 	
 	public String compareSub(ArrayList<String>codons, String peptide){	  
-		 if (i == codons.size()-1) {   
+		 if (i == codons.size()) {   
 			    System.out.print(peptide.toString());   
 		    }
 		 
-		  while( i < codons.size()-1) {  
+		  while( i <= codons.size()-1) {  
 		  if (codons.get(i).equals("UGC")) {
 				singleLetterCode = "C";
 			     proteinSequence(peptide, singleLetterCode);
