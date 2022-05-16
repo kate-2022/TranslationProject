6974b62f5f41b4049ccb675ffc74e8a0f2fdc8c3
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-05-16 21:37:39 +0200

    Update Prolin.java

--- Prolin.java
+++ Prolin.java
@@ -21,11 +21,11 @@ public class Prolin extends AminoAcids implements Translatable {
 	}
 	
 	public String compareSub(ArrayList<String>codons, String peptide){	  
-		 if (i == codons.size()-1) {   
+		 if (i == codons.size()) {   
 			    System.out.print(peptide.toString());   
 		     }
 		 
-		  while( i < codons.size()-1) {  
+		  while( i <= codons.size()-1) {  
 		  if (codons.get(i).equals("CCA")) {
 				singleLetterCode = "P";
 			     proteinSequence(peptide, singleLetterCode);
